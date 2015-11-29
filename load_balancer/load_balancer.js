fe41995a57f48f912833acf18df17f742d2bb45b
Aakash Mangal <aakash.mangal77@gmail.com>
2015-11-29 15:04:23 -0800

    updated load balancing and server down handling

--- load_balancer/load_balancer.js
+++ load_balancer/load_balancer.js
@@ -1,6 +1,13 @@
-var http = require('http');
-var fs = require('fs');
-var proxy = require('http-proxy');
+var http = require('http'),
+  fs = require('fs'),
+  proxy = require('http-proxy'),
+  request = require('request');
+
+
+var TIMEOUT = 5000,
+  failoverTimer=[],
+  RETRY = 1;
+
 
 http.globalAgent.maxSockets = 10240;
 
@@ -14,29 +21,110 @@ var proxies = servers.map(function (target) {
   return new proxy.createProxyServer({
     target: target,
     ws: true,
-    xfwd: true
+    xfwd: true,
+    down: false
   });
 });
 
 /**
- * Select a random server to proxy to.
- * @return {Number}     Index of the proxy to use.
+ * Select a up and running server randomnly
  */
 var selectServer = function(req, res) {
-  return Math.floor(Math.random() * proxies.length);
+  //Randomnly selected
+  var selectedServer = Math.floor(Math.random() * proxies.length);
+
+  //check if the selected server is down
+  if(proxies[selectedServer].options.down){
+    selectedServer = -1;
+
+    var attempts = 0;
+    while(attempts < proxies.length && selectedServer<0)
+    {
+      var newSelectedServer = Math.floor(Math.random()*proxies.length);
+      if(!proxies[newSelectedServer].options.down){
+        selectedServer = newSelectedServer;
+        attempts = proxies.length;
+      }
+      attempts++;
+    }
+  }
+
+  return selectedServer;
 };
+
+var retries = 0;
+//Handle Server failure
+var requestFailHandler = function(index,req,res) {
+  if (failoverTimer[index]) {
+    return;
+  }
+  if(retries >= RETRY){
+    renderErrorPage(res);
+    return ;
+  }
+
+  failoverTimer[index] = setTimeout(function() {
+    // Check if the server is up or not
+    request({
+      url: 'http://' + proxies[index].options.target.host + ':' + proxies[index].options.target.port,
+      method: 'HEAD',
+      timeout: TIMEOUT
+    }, function(err, resp, body) {
+      failoverTimer[index] = null;
+      if (resp && resp.statusCode === 200) {
+        proxies[index].options.down = false;
+        console.log('Server ' + proxies[index].options.target.host + ' is back up.');
+        retries=0;
+        proxyRequest(req,res,index);
+        return ;
+      } else {
+        proxies[index].options.down = true;
+        retries++;
+        console.log('Server ' + proxies[index].options.target.host + ' is still down.');
+        requestFailHandler(index,req,res);
+      }
+    });
+  }, TIMEOUT);
+};
+
+//Render Error Page
+var renderErrorPage = function(response){
+    fs.readFile('./views/error.html',function(err,html){
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);
+        response.end();
+    });
+};
+
+
+//Proxy the incoming request
+var proxyRequest = function(req,res,index){
+
+  var proxy = proxies[index];
+
+  if(proxy){
+    console.log("Redirecting request at "+proxy.options.target.host);
+    proxy.web(req, res);
+    proxy.on('error', function(err) {
+      requestFailHandler(index,req,res);
+    });  
+  }
+};
+
 
 // Select the next server and send the http request.
-var serverCallback = function(req, res) {
+var server = http.createServer(function(req, res) {
   var proxyIndex = selectServer();
-  var proxy = proxies[proxyIndex];
-  proxy.web(req, res);
-
-  proxy.on('error', function(err) {
-    startFailoverTimer(proxyIndex);
-  });
-};
-var server = http.createServer(serverCallback);
+  //If no server found
+  if(proxyIndex<0){
+    renderErrorPage(res);
+  }
+  //If server allotted
+  proxyRequest(req,res,proxyIndex);
+  
+}).listen(8080,function(){
+  console.log("Load Balancer started at 8080");
+});
 
 // Get the next server and send the upgrade request.
 server.on('upgrade', function(req, socket, head) {
@@ -46,7 +134,6 @@ server.on('upgrade', function(req, socket, head) {
 
   proxy.on('error', function(err, req, socket) {
     socket.end();
-    startFailoverTimer(proxyIndex);
+    requestFailHandler(proxyIndex,null);
   });
 });
-server.listen(8080);
